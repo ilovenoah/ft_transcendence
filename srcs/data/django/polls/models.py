dba0c4a21cc8121e65e05abc9158086b02259a48
kohshi54 <kohshi54.yam@gmail.com>
2024-05-06 01:10:53 +0900

    customize admin page

--- srcs/data/django/polls/models.py
+++ srcs/data/django/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 from django.db import models
@@ -10,6 +11,11 @@ class Question(models.Model):
 	pub_date = models.DateTimeField("date published")
 	def __str__(self):
 		return self.question_text
+	@admin.display(
+		boolean=True,
+		ordering="pub_date",
+		description="Published recently?",
+	)
 	def was_published_recently(self):
 		now = timezone.now()
 		return now - datetime.timedelta(days=1) <= self.pub_date <= now
