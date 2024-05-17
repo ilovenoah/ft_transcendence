5a33588eadd22961db63b100dff8b8fa21684cda
Captain Rakko <tyonezaw@student.42tokyo.jp>
2024-05-17 20:56:49 +0900

    20240517

--- srcs/data/django/main/urls.py
+++ srcs/data/django/main/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('page1/', views.page1, name='page1'),
     path('page2/', views.page2, name='page2'),
     path('page3/', views.page3, name='page3'),
+    path('ponggame/', views.ponggame, name='ponggame'),
 ]
--- srcs/data/django/main/views.py
+++ srcs/data/django/main/views.py
@@ -8,6 +8,8 @@ def page2(request):
     return render(request, 'page2.html')
 def page3(request):
     return render(request, 'page3.html')
+def ponggame(request):
+    return render(request, 'ponggame.html')
 
 
 
