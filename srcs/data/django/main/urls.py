d75d99647c96464c1dcce383eb93c95f29ece1d2
前川素絵 <maekawamotoe@MacBook-Air-6.local>
2024-05-20 23:19:55 +0900

    logout

--- srcs/data/django/main/urls.py
+++ srcs/data/django/main/urls.py
@@ -3,9 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from main import views
 
-# from .views import signup
 from django.contrib.auth import views as auth_views
-# from .views import profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,13 +13,8 @@ urlpatterns = [
     path('page3/', views.page3, name='page3'),
     path('ponggame/', views.ponggame, name='ponggame'),
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user=True), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('accounts/profile/', views.profile, name='profile'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
 ]
 
-
-# urlpatterns = [
-#     path('signup/', signup, name='signup'),
-#     path('login/', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user=True, success_url='/home/'), name='login'),
-#     path('accounts/profile/', profile, name='profile'),
-# ]
