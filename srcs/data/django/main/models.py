184f794f971f706ae831d2d7830f924986984427
前川素絵 <maekawamotoe@MacBook-Air-6.local>
2024-05-24 13:16:35 +0900

    added default nickname and avatar

--- srcs/data/django/main/models.py
+++ srcs/data/django/main/models.py
@@ -3,7 +3,16 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     nickname = models.CharField(max_length=255, null=True, blank=True)
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    avatar = models.ImageField(
+        upload_to='avatars/', 
+        null=True, 
+        blank=True,
+        default='avatars/default.png'  # デフォルト画像のパスを指定
+    )
 
+    def save(self, *args, **kwargs):
+        if not self.nickname:  # nicknameが空の場合
+            self.nickname = self.username  # usernameをnicknameに設定
+        super().save(*args, **kwargs)
 
 # Create your models here.
