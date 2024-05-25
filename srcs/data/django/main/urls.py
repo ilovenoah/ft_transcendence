438a6f0bda62acd63122b99fec987a7c84fea47b
rakkotan <rakkotan@gmail.com>
2024-05-25 22:18:49 +0900

    202405252218

--- srcs/data/django/main/urls.py
+++ srcs/data/django/main/urls.py
@@ -5,7 +5,6 @@ from main import views
 from .views import process_post_data
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('process-post/', process_post_data, name='process_post_data'),
 ]
