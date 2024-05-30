0d1283097d491163bd7bd8ce982c4a6789956923
前川素絵 <maekawamotoe@MacBook-Air-6.local>
2024-05-31 01:17:39 +0900

    fixed profile

--- srcs/data/django/team_my2/urls.py
+++ srcs/data/django/team_my2/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dev/', include('main.urls')),  
+    path('', include('main.urls')),  
 ]
 
 
