e99800f7c8b2b665816526b4b4a6fe14131aa79e
前川素絵 <maekawamotoe@MacBook-Air-6.local>
2024-06-05 17:01:11 +0900

    fixed change_password

--- srcs/data/django/main/forms.py
+++ srcs/data/django/main/forms.py
@@ -39,21 +39,26 @@ class AvatarForm(forms.ModelForm):
         widgets = {
             'avatar': forms.HiddenInput(),
         }
-        error_messages = {
-            'avatar': {
-                'required': "",  # カスタムエラーメッセージ
-            }
-        }
     def __init__(self, *args, **kwargs):
         super(AvatarForm, self).__init__(*args, **kwargs)
         self.fields['avatar'].required = True
 
 class PasswordChangeForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput(), label="Confirm password")
 
     class Meta:
         model = User
         fields = ['password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            self.add_error('confirm_password', "Passwords do not match.")
+        return cleaned_data
 
     def save(self, commit=True):
         user = super().save(commit=False)
