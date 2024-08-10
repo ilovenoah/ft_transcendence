fb098e7ce9578d01a9f186130c09b32154b04ebe
mewubuntu <rakkotan@gmail.com>
2024-08-10 15:10:55 +0900

    fix_language_to_backbutton

--- srcs/data/django/main/static/js/app.js
+++ srcs/data/django/main/static/js/app.js
@@ -146,6 +146,8 @@ window.addEventListener("popstate", function(event) {
   if (event.state) {
     // 状態オブジェクトが存在する場合、表示内容を更新
     updateContent(event.state.data);
+
+
   }
 });
   
@@ -228,7 +230,12 @@ function updateContent(data) {
       // <foot> タグに新しいスクリプトタグを挿入
       foot.appendChild(arrayScript);
     }
+
+
   }
+
+
+  loadLanguage();
   
   // var links = document.getElementById('content').querySelectorAll(".post-link");  
   // links.forEach(function(link) {
